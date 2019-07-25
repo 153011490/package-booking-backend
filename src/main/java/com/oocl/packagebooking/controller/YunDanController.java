2eb524d86a43e97d8563a3444c8a4f8a7b4b51fe
SamsonLin <153011490@qq.com>
2019-07-25 10:53:01 +0800

    feat:complete addYundan and findAll

---
+++ src/main/java/com/oocl/packagebooking/controller/YunDanController.java
@@ -0,0 +1,32 @@
+package com.oocl.packagebooking.controller;
+
+import com.oocl.packagebooking.entity.YunDan;
+import com.oocl.packagebooking.service.YunDanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/yundanhao")
+public class YunDanController {
+
+    @Autowired
+    private YunDanService yunDanService;
+
+    @PostMapping
+    public String addYunDan(@RequestBody YunDan yunDan){
+        try{
+            yunDanService.addYunDan(yunDan);
+            return "添加成功";
+        }catch (Exception e){
+            return "添加失败";
+        }
+    }
+
+    @GetMapping
+    public List<YunDan> findAll(){
+        return yunDanService.findAll();
+    }
+
+}
---
+++ src/main/java/com/oocl/packagebooking/respository/YunDanReposity.java
@@ -0,0 +1,7 @@
+package com.oocl.packagebooking.respository;
+
+import com.oocl.packagebooking.entity.YunDan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface YunDanReposity extends JpaRepository<YunDan,String> {
+}
